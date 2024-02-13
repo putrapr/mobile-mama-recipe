14e719c2de77790210c35b7dc831ac97d584e4ab
Putra Prasetya <putraprasetya333@gmail.com>
2024-02-13 10:34:33 +0700

    Setting eslint, env, & Instal Dependencies

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,4 +1,10 @@
 module.exports = {
   root: true,
   extends: '@react-native',
-};
+  rules: {
+    'prettier/prettier': 0,
+    semi: ['error', 'never'],
+    quotes: ['error', 'single', { 'avoidEscape': true }],
+    indent: ['error', 2],
+  },
+}
